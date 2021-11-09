a592b0ab0388dddf431d06853d948f7c3924ec6b
Brahim Berkasse <bberkass@e1r9p11.1337.ma>
2021-11-09 13:46:04 +0100

    FIX : integer to ascii; fix n = 0 case

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 14:50:02 by bberkass          #+#    #+#             */
-/*   Updated: 2021/11/06 12:55:12 by bberkass         ###   ########.fr       */
+/*   Updated: 2021/11/09 13:43:31 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,8 @@ static int	ft_nsize(int n)
 {
 	int	size;
 
-	size = 0;
-	if (n < 0)
-		n = n * -1;
-	while (n > 0)
+	size = 1;
+	while (n > 9)
 	{
 		n = n / 10;
 		size++;
@@ -40,17 +38,21 @@ char	*ft_itoa(int n)
 		n = n * -1;
 		negative = 1;
 	}
-	size = ft_nsize(n);
-	a = (char *)malloc((size + negative + 1) * sizeof(char));
+	size = ft_nsize(n) + negative;
+	if(!(a = (char *)malloc((size + 1) * sizeof(char))))
+			return (0);
 	if (negative)
 		a[0] = '-';
-	i = size - 1 + negative;
+	i = size - 1;
+	if(n == 0)
+		a[i] = '0';
 	while (n > 0)
 	{
 		a[i] = '0' + n % 10;
 		n = n / 10;
 		i--;
 	}
+	
 	a[size] = '\0';
 	//printf("a => %s \n", a);
 	return (a);
