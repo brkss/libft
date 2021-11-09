4f6461a0503ca793f134d0cc281bc8ef08f4d9ed
Brahim Berkasse <bberkass@e1r9p11.1337.ma>
2021-11-09 14:18:34 +0100

    FIX : add to last node in linked list

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 12:45:52 by bberkass          #+#    #+#             */
-/*   Updated: 2021/11/08 12:53:36 by bberkass         ###   ########.fr       */
+/*   Updated: 2021/11/09 14:01:36 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*p;
 	
-	if(!lst || !new)
-		return;
-	p = *lst;
-	while(p && p->next)
-		p = p->next;
-	
-	p->next = new;
+	if(*lst)
+	{	
+		p = *lst;
+		while(p->next)
+			p = p->next;
+		p->next = new;
+	}
+	else {
+		*lst = new;
+	}
 }
