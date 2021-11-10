f68147c1544f350c23549b63b7abc223df353448
Brahim Berkasse <bberkass@e1r9p11.1337.ma>
2021-11-10 22:06:26 +0100

    CHORE : MAP INTO LINKED LIST

---
+++ ft_lstmap.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/10 21:15:00 by bberkass          #+#    #+#             */
+/*   Updated: 2021/11/10 21:26:00 by bberkass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*head;
+	t_list	*node;
+	t_list	*tmp;
+
+	if (!lst)
+		return (NULL);
+	head = ft_lstnew(f(lst->content));
+	node = head;
+	lst = lst->next;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&head, del);
+			return (NULL);
+		}
+		node->next = tmp;
+		node = node->next;
+		lst = lst->next;
+	}
+	return (head);
+}
