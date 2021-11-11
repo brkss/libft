531d15cbec4f0c55cea7fa60c61bc580c9b8136e
Brahim Berkasse <bberkass@e1r9p11.1337.ma>
2021-11-11 01:11:49 +0100

    FIX : Sub string dont allocate the right size !

--- ft_substr.c
+++ ft_substr.c
@@ -6,26 +6,38 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 10:55:11 by bberkass          #+#    #+#             */
-/*   Updated: 2021/11/11 01:02:59 by bberkass         ###   ########.fr       */
+/*   Updated: 2021/11/11 01:11:15 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	len_byindex(const char *s, int index)
+{
+	int	l;
+
+	l = index;
+	while (s[l])
+		l++;
+	return (l - index);
+}
 
 char	*ft_substr(char const *s, size_t start, size_t len)
 {
 	size_t			isub;
 	size_t			isrc;
 	char			*sub;
+	int				l;
 
 	isub = 0;
 	isrc = start;
+	l = len_byindex(s, start);
 	if (!s)
 		return (0);
 	if (start >= ft_strlen(s))
 		sub = (char *)malloc(sizeof(char));
 	else
-		sub = (char *)malloc((len + 1) * sizeof(char));
+		sub = (char *)malloc((l + 1) * sizeof(char));
 	if (!sub)
 		return (0);
 	while (s[isrc] && isub < len && start < ft_strlen(s))
